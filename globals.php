47e9c7012ca5eebfc78866516f495a6ec8665b30
Graham Watson <grahamj.watson@gmail.com>
2016-12-06 22:38:22 +0000

    Initial Commit

---
+++ globals.php
@@ -0,0 +1,10 @@
+<?php
+//
+// globals.php - db connection and other general information
+//
+    $hostName = "10.169.0.121";
+    $database = "shinyide2_lottery";
+    $userName = "shinyide2_ro";
+    $password = "R3l9c675";
+
+?>

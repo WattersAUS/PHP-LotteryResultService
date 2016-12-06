4b7bc8c979b4f3aabda4895059fc381f482ed00c
Graham Watson <grahamj.watson@gmail.com>
2016-12-06 22:48:09 +0000

    Updated

--- globals.php
+++ globals.php
@@ -2,9 +2,9 @@
 //
 // globals.php - db connection and other general information
 //
-    $hostName = "10.169.0.121";
-    $database = "shinyide2_lottery";
-    $userName = "shinyide2_ro";
-    $password = "R3l9c675";
+    $hostName = "";
+    $database = "";
+    $userName = "";
+    $password = "";
 
 ?>
